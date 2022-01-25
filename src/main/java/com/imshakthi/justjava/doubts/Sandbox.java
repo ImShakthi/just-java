d9cdc7e4cd0bc69ef94872ab287dae687a8039ec
Sakthivel B <ideamatex@gmail.com>
2022-01-25 23:40:36 +0530

    convert to maven project

--- src/main/java/com/imshakthi/justjava/doubts/Sandbox.java
+++ src/main/java/com/imshakthi/justjava/doubts/Sandbox.java
@@ -1,5 +1,7 @@
 package com.imshakthi.justjava.doubts;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.imshakthi.justjava.doubts.impl.OperationEvent;
 import com.imshakthi.justjava.doubts.interfaces.Event;
 
@@ -7,5 +9,16 @@ public class Sandbox {
     public static void main(String[] args) {
         Event event = new OperationEvent();
         System.out.println(event.getUsername());
+
+        dummy(event);
+    }
+
+    private static void dummy(Object event) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            System.out.println(objectMapper.writeValueAsString(event));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/imshakthi/leetcode/blind75/P001TwoSum.java
@@ -0,0 +1,7 @@
+package com.imshakthi.leetcode.blind75;
+
+public class P001TwoSum {
+    public static void main(String[] args) {
+        System.out.println("Observer pattern");
+    }
+}
