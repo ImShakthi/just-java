b063513de255006566d9b109472b62a31afebdd3
Sakthivel B <ideamatex@gmail.com>
2022-01-25 23:47:28 +0530

    fix main package

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-package com.imshakthi;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/com/imshakthi/Main.java
@@ -0,0 +1,7 @@
+package com.imshakthi;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
